d62e26b954614458bba6e2addfbfb948d21e519c
Oleksandr Poliarush <oleksandr.poliarush@sigma.software>
2019-03-21 12:32:44 +0200

    initail commit

---
+++ app.js
@@ -0,0 +1,10 @@
+import express from 'express';
+const port = process.env.PORT || 3000;
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+app.listen(port, () => console.log(`Running on port ${port}`));

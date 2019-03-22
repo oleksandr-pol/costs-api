9db196089607c0dc7141d0929b8806ce3cbafbf8
Oleksandr Poliarush <oleksandr.poliarush@sigma.software>
2019-03-22 10:29:47 +0200

    set-up basic router

--- app.js
+++ app.js
@@ -2,6 +2,15 @@ import express from 'express';
 const port = process.env.PORT || 3000;
 
 const app = express();
+const costRouter = express.Router();
+
+costRouter.route('/costs')
+  .get((req, res) => {
+    const response = { hello: 'This is API' };
+    return res.json(response);
+  });
+
+app.use('/api', costRouter);
 
 app.get('/', (req, res) => {
   res.send('Hello World');

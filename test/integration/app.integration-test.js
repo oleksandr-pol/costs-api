038c5931802e04334b1f7b68ea3b60426352651d
Oleksandr Poliarush <oleksandr.poliarush@sigma.software>
2019-04-05 18:22:14 +0300

    add more integration tests

--- test/integration/app.integration-test.js
+++ test/integration/app.integration-test.js
@@ -1,9 +1,15 @@
 import config from '../../config';
 import server from '../../src/app';
-import request from 'request';
+import rp from 'request-promise-native';
 import mongoose from 'mongoose';
 import Cost from '../../src/db/costModel';
 import { expect } from 'chai';
+
+const mockCost = {
+  value: 100,
+  title: 'Cost',
+  type: 'Games'
+};
 
 describe('server api', () => {
   beforeEach(() => {
@@ -11,32 +17,157 @@ describe('server api', () => {
   });
 
   describe('GET:/api/costs', () => {
-    it('should return costs array', done => {
-      const mockCost = {
-        value: 100,
-        title: 'Cost',
-        type: 'Games'
-      };
+    it('should return costs array', async () => {
+      try {
+        await new Cost(mockCost).save();
 
-      new Cost(mockCost).save().then(() => {
-        request(`${config.getAppUrl()}/api/costs`, (err, res, body) => {
-          if (err) {
-            return done(err);
-          }
-          const response = JSON.parse(body);
+        let res = await rp(`${config.getAppUrl()}/api/costs`);
+        res = JSON.parse(res);
 
-          expect(response.length).to.equal(1);
-          expect(mockCost.value).to.equal(response[0].value);
-          done();
-        })
-      }).catch(err => done(err));
+        expect(res.length).to.equal(1);
+        expect(mockCost.value).to.equal(res[0].value);
+      } catch (e) {
+        console.error(e);
+        expect(true).to.be.false;
+      }
     });
 
-    it('should throw 400 error if query is not valid', () => {
-
+    it('should throw 400 error if query is not valid', async () => {
+      try {
+        await rp(`${config.getAppUrl()}/api/costs?test=1`);
+        expect(true).to.be.false;
+      } catch (e) {
+        expect(e.statusCode).to.equal(400);
+      }
     });
   });
 
+  describe('POST:/api/costs', () => {
+    it('should save new cost to db', async () => {
+      try {
+        await rp({
+          method: 'POST',
+          uri: `${config.getAppUrl()}/api/costs`,
+          body: mockCost,
+          json: true
+        });
+
+        let saved = await rp(`${config.getAppUrl()}/api/costs`);
+        saved = JSON.parse(saved);
+
+        expect(saved.length).to.equal(1);
+        expect(mockCost.value).to.equal(saved[0].value);
+      } catch (e) {
+        console.error(e);
+        expect(true).to.be.false;
+      }
+    });
+
+    it('should throw 400 error if title is not provided', async () => {
+      try {
+        await rp({
+          method: 'POST',
+          uri: `${config.getAppUrl()}/api/costs`,
+          body: {
+            value: 100,
+            type: 'Games'
+          },
+          json: true
+        });
+        expect(true).to.be.false;
+      } catch (e) {
+        expect(e.statusCode).to.equal(400);
+      }
+    });
+
+    it('should throw 400 error if value is not provided', async () => {
+      try {
+        await rp({
+          method: 'POST',
+          uri: `${config.getAppUrl()}/api/costs`,
+          body: {
+            type: 'Games',
+            title: 'Cost',
+          },
+          json: true
+        });
+        expect(true).to.be.false;
+      } catch (e) {
+        expect(e.statusCode).to.equal(400);
+      }
+    });
+
+    it('should throw 400 error if type is not provided', async () => {
+      try {
+        await rp({
+          method: 'POST',
+          uri: `${config.getAppUrl()}/api/costs`,
+          body: {
+            title: 'Cost',
+            value: 100,
+          },
+          json: true
+        });
+        expect(true).to.be.false;
+      } catch (e) {
+        expect(e.statusCode).to.equal(400);
+      }
+    });
+
+    it('should throw 400 error if type longer then 50 symbols', async () => {
+      try {
+        await rp({
+          method: 'POST',
+          uri: `${config.getAppUrl()}/api/costs`,
+          body: {
+            title: 'Cost',
+            value: 100,
+            type: new Array(51).fill('a')
+          },
+          json: true
+        });
+        expect(true).to.be.false;
+      } catch (e) {
+        expect(e.statusCode).to.equal(400);
+      }
+    });
+
+    it('should throw 400 error if type has special symbols', async () => {
+      try {
+        await rp({
+          method: 'POST',
+          uri: `${config.getAppUrl()}/api/costs`,
+          body: {
+            title: 'Cost',
+            value: 100,
+            type: ', .'
+          },
+          json: true
+        });
+        expect(true).to.be.false;
+      } catch (e) {
+        expect(e.statusCode).to.equal(400);
+      }
+    });
+
+    it('should throw 400 error if type length is less then 2 symbols', async () => {
+      try {
+        await rp({
+          method: 'POST',
+          uri: `${config.getAppUrl()}/api/costs`,
+          body: {
+            title: 'Cost',
+            value: 100,
+            type: 'a'
+          },
+          json: true
+        });
+        expect(true).to.be.false;
+      } catch (e) {
+        expect(e.statusCode).to.equal(400);
+      }
+    });
+  });
 
   after(() => {
     mongoose.connection.close();
